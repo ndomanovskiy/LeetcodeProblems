84df41805d1698f0c4fbbd477516ab611bb0b3dd
Nikita <ndomanovskiy@gmail.com>
2022-05-04 22:57:43 +0300

    added solution for LC8 String to Integer

--- tests/Leetsolu.Tests/SolutionsTests.cs
+++ tests/Leetsolu.Tests/SolutionsTests.cs
@@ -71,4 +71,20 @@ public class SolutionsTests
     {
         Assert.Equal(result, Solution.Reverse(x));
     }
+
+    [Theory]
+    [InlineData("123", 123)]
+    [InlineData("-42", -42)]
+    [InlineData("   -42", -42)]
+    [InlineData("4193 with words", 4193)]
+    [InlineData("-91283472332", int.MinValue)]
+    [InlineData("words and 987", 0)]
+    [InlineData("3.14159", 3)]
+    [InlineData("+-12", 0)]
+    [InlineData("   +0 123", 0)]
+    [InlineData("  +  413", 0)]
+    public void LC8StringtoInteger(string s, int result)
+    {
+        Assert.Equal(result, Solution.MyAtoi(s));
+    }
 }
